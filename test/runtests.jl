1db45adc73fce80838f6a622b0c9ff19f3d9783f
raphasampaio <raphaelaraujosampaio@gmail.com>
2024-11-07 22:07:29 -0300

    Initial commit

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,12 +3,27 @@ using ProgressTables
 using Aqua
 using Test
 
-include("aqua.jl")
+# include("aqua.jl")
 
 function test_all()
-    @testset "Aqua.jl" begin
-        test_aqua()
+    # @testset "Aqua.jl" begin
+    #     test_aqua()
+    # end
+
+    @show pt = ProgressTable(
+        header = ["Epoch", "Loss", "Accuracy"],
+        widths = [10, 8, 16],
+        format = ["%d", "%.2f", "%.3e"],
+        color = [:normal, :normal, :green],
+    )
+
+    initialize!(pt)
+
+    for epoch in 1:10
+        next!(pt, [epoch, 0.1, 0.9])
     end
+
+    finalize!(pt)
 
     return nothing
 end
