8d39ec809522920f93fb2d879667bbe7fdf20a73
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-08-23 23:35:58 -0300

    Update

--- src/ProgressTables.jl
+++ src/ProgressTables.jl
@@ -15,5 +15,6 @@ include("abstract.jl")
 include("style.jl")
 include("incremental_table.jl")
 include("incremental_separator.jl")
+include("stdout.jl")
 
 end
---
+++ src/stdout.jl
@@ -0,0 +1,29 @@
+function initialize(progress_table::AbstractProgressTable)
+    ProgressTables.initialize(stdout, progress_table)
+    return nothing
+end
+
+function next(progress_table::AbstractProgressTable, row::AbstractVector; kwargs...)
+    ProgressTables.next(stdout, progress_table, row; kwargs...)
+    return nothing
+end
+
+function separator(progress_table::AbstractProgressTable)
+    ProgressTables.separator(stdout, progress_table)
+    return nothing
+end
+
+function Base.finalize(progress_table::AbstractProgressTable)
+    ProgressTables.finalize(stdout, progress_table)
+    return nothing
+end
+
+function next!(separator::AbstractSeparator)
+    ProgressTables.next!(stdout, separator)
+    return nothing
+end
+
+function finalize!(separator::AbstractSeparator)
+    ProgressTables.finalize!(stdout, separator)
+    return nothing
+end
