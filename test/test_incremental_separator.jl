8dea8774eb500a74caddb298d861181e1916d45d
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-08-24 00:28:31 -0300

    Update

--- test/test_incremental_separator.jl
+++ test/test_incremental_separator.jl
@@ -4,19 +4,32 @@ using ProgressTables
 using Test
 
 @testset "Incremental Separator" begin
-    io = IOBuffer()
+    @testset "string > max_steps" begin
+        io = IOBuffer()
 
-    separator = IncrementalSeparator("Hello, World!", 5)
+        max_steps = 5
+        separator = IncrementalSeparator("Hello, World!", max_steps)
+        for _ in 1:max_steps
+            next(io, separator)
+        end
+        finalize(io, separator)
 
-    for _ in 1:5
-        next(io, separator)
+        @show output = String(take!(io))
+        print(output)
+
+        @test output == "Hello, World!"
     end
-    finalize(io, separator)
 
-    @show output = String(take!(io))
-    print(output)
+    @testset "string < max_steps" begin
+        io = IOBuffer()
 
-    @test output == "Hello, World!"
+        max_steps = 5
+        separator = IncrementalSeparator("Hello, World!", 20)
+        for _ in 1:5
+            next(io, separator)
+        end
+        finalize(io, separator)
+    end
 end
 
 end
