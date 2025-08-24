f4faf9d0f30bb055f1c2d735fdb5d61eeac91b44
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-08-24 01:13:54 -0300

    Update

--- test/test.bat
+++ test/test.bat
@@ -2,8 +2,4 @@
 
 SET BASEPATH=%~dp0
 
-IF "%~1"=="" (
-    CALL julia +1.11 --project=%BASEPATH%\.. -e "import Pkg; Pkg.test()"
-) ELSE (
-    CALL julia +1.11 --project=%BASEPATH%\.. %BASEPATH%\runtests.jl %1
-)
+CALL julia +1.11 --project=%BASEPATH%\.. -e "import Pkg; Pkg.test(test_args=ARGS)" -- %*
--- test/test_stdout.jl
+++ test/test_stdout.jl
@@ -4,15 +4,15 @@ using ProgressTables
 using Test
 
 @testset "Stdout" begin
-    pt = IncrementalProgressTable(header=["Test"], widths=[5])
-    sep = IncrementalSeparator(width=10)
+    pt = IncrementalProgressTable(header=["Test"], format = ["%d"])
+    sp = IncrementalSeparator(pt, 10)
     
     @test initialize(pt) === nothing
-    @test next(pt, ["A"]) === nothing  
+    @test next(pt, [1]) === nothing  
     @test separator(pt) === nothing
     @test finalize(pt) === nothing
-    @test next(sep) === nothing
-    @test finalize(sep) === nothing
+    @test next(sp) === nothing
+    @test finalize(sp) === nothing
 end
 
 end
